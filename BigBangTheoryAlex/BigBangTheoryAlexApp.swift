0e2484554bf7a33120615c914a3547cd69c266c4
alexmaxu <12alexmaxu@gmail.com>
2024-04-16 13:20:12 +0200

    Initial Commit

---
+++ BigBangTheoryAlex/BigBangTheoryAlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangTheoryAlexApp.swift
+//  BigBangTheoryAlex
+//
+//  Created by Alex  on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BigBangTheoryAlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
